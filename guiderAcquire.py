a8b51fa8db940f1f1ca5f2211e6c466e4d08127e
jdpelletier <john.pelletier@ymail.com>
2020-04-28 09:42:52 -1000

    Update guiderAcquire.py

--- guiderAcquire.py
+++ guiderAcquire.py
@@ -13,45 +13,12 @@ instrument = dcs.read('INSTRUME')
 instService = ktl.Service(instrument)
 gscale = instService.read('gscale')
 
-def wftel():
-    dcs = ktl.Service('dcs')
-    autresum = dcs.read('autresum')
-    startTime = time.time()
-    axestat = dcs.monitor('AXESTAT')
-    ktl.waitfor(axestat == "tracking")
-    active = dcs.read("AUTACTIV")
-    if (active == 'no'):
-        print("WARNING: guider not currently active.\n")
-        return
-    count = 0
-    while(True):
-        if autresum != dcs.read('autresum'):
-            break
-        count += 1
-        if count >= 20:
-            print("[wftel] WARNING: timeoutwaiting for AUTRESUM to increment\n\a")
-            break
-        time.sleep(1)
-    count = 0
-    while(true):
-        autgo = dcs.read('autgo')
-        if autgo.upper() == "RESUMEACK" or augo.upper() == "GUIDE":
-            break
-        count += 1
-        if count >= 20:
-            print("[wftel]WARNING: timeout waiting for AUTGO to be RESUMEACK or GUIDE\n\a")
-            break
-        time.sleep(1)
-    elapsedTime = time.time() - startTime
-    return elapsedTime
-
 def gxy(x, y):
     tvxoff = self.dcs['tvxoff']
     tvyoff = self.dcs['tvyoff']
     tvxoff.write(x, rel2curr = 't')
     tvyoff.write(y, rel2curr = 't')
-    elapsedTime = wftel()
-    print("[gxy] wftel completed in %f sec" % elapsedTime)
+    print("Offset executed")
     return True
 
 
